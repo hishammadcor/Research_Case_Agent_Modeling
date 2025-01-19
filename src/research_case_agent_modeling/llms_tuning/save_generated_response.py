4553a026d7e48456dff52988bfbca194278dfc18
Hisham Madcor <hisham3madcor@gmail.com>
2025-01-19 13:54:24 +0100

    Edit loading and saving responces functuions

--- src/research_case_agent_modeling/llms_tuning/save_generated_response.py
+++ src/research_case_agent_modeling/llms_tuning/save_generated_response.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def save_responses_to_csv(responses, output_file):
     """
@@ -9,3 +10,14 @@ def save_responses_to_csv(responses, output_file):
         writer.writerow(["Question_Tag", "Response"])  
         for tag, response in responses.items():
             writer.writerow([tag, response])
+
+
+def save_responses_to_json(responses, output_file):
+    """
+    Saves LLM responses to a JSON file.
+    """
+    try:
+        with open(output_file, 'w', encoding='utf-8') as file:
+            json.dump(responses, file, indent=4, ensure_ascii=False)
+    except Exception as e:
+        print(f"Error saving responses to JSON: {e}")
